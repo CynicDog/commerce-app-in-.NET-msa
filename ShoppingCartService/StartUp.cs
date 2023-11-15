97b8280519cfbbfcd8d375fd1db6da8f0714f555
ginsenglee <thecynicdog0328@gmail.com>
2023-11-15 13:18:04 +0900

    transient dependency injection / initialization task performance

--- ShoppingCartService/StartUp.cs
+++ ShoppingCartService/StartUp.cs
@@ -1,3 +1,6 @@
+using ShoppingCartService.Business.store;
+using ShoppingCartService.Business.store.impl;
+
 namespace ShoppingCartService 
 {
     public class Startup
@@ -5,6 +8,12 @@ namespace ShoppingCartService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // dependency injection lifetime options: 
+            //      1. Transient: A new instance is created every time the service is requested.
+            //      2. Scoped: A single instance is created for each scope. A scope is usually equivalent to the lifetime of a single HTTP request.
+            //      3. Singleton: A single instance is created for the entire lifetime of the application. 
+            services.AddTransient<IShoppingCartStore, ShoppingCartStore>(); 
         }
  
         public void Configure(IApplicationBuilder app)
@@ -19,6 +28,9 @@ namespace ShoppingCartService
             //          Action greet = () => Console.WriteLine("Hello, World!");
             //          greet();
             //       `
+
+            var shoppingCartStore = app.ApplicationServices.GetService<IShoppingCartStore>();
+            shoppingCartStore?.Init();
         }
     }
 }
