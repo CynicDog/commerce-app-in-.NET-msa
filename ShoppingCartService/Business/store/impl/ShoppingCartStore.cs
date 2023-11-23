87753cc6602537d380a7989df8e23fee9a258750
ginsenglee <thecynicdog0328@gmail.com>
2023-11-23 22:03:04 +0900

    added loggings

--- ShoppingCartService/Business/store/impl/ShoppingCartStore.cs
+++ ShoppingCartService/Business/store/impl/ShoppingCartStore.cs
@@ -6,9 +6,15 @@ namespace ShoppingCartService.Business.store.impl;
 
 public class ShoppingCartStore : IShoppingCartStore
 {
+    private readonly ILogger Logger; 
     private string connectionTemplate =
         @"Data Source=localhost;Initial Catalog=ShoppingCart;User Id=sa;Password=yourStrongPassword!";
-    
+
+    public ShoppingCartStore(ILogger<ShoppingCartStore> logger)
+    {
+        this.Logger = logger;
+    }
+
     public async Task<ShoppingCart> Get(int userId)
     {
         await using var connection = new SqlConnection(this.connectionTemplate);
@@ -19,6 +25,7 @@ public class ShoppingCartStore : IShoppingCartStore
 
         // When the rows returned by a SQL query have column names equal to the property names in a class,
         // Dapper can automatically map to instances of the class.
+        Logger.LogInformation("Fetching row(s) with the user identifier of [{@}]", userId);
         return new ShoppingCart(
             founds.FirstOrDefault()?.ID, 
             userId, 
@@ -37,29 +44,32 @@ public class ShoppingCartStore : IShoppingCartStore
         await connection.OpenAsync();
         await using (var transaction = connection.BeginTransaction())
         {
-            // `output` sql clause returns the generated identity value for the inserted row   
+            // `output` sql clause returns the generated identity value for the inserted row
             var shoppingCartId = shoppingCart.Id ?? // null-coalescing operator 
                                  await connection.QuerySingleAsync<int>(
                                      SqlQueries.insertShoppingCartSql, 
                                      new { shoppingCart.UserId }, transaction
-                                     );
+                                 );
+            Logger.LogInformation("Inserted an instance of `ShoppingCart` with the identifier of [{@}]", shoppingCart.Id);
 
-            // delete all previous shopping cart items  
+            // delete all previous shopping cart items
+            Logger.LogInformation("Deleting an instance of `ShoppingCart` with the identifier of [{@}]", shoppingCart.Id);
             await connection.ExecuteAsync(
                 SqlQueries.deleteForShoppingCartSql,
                 new { UserId = shoppingCart.UserId }, transaction);
 
-            // add current shopping cart items 
+            // add current shopping cart items
+            Logger.LogInformation("Inserting an instance of `ShoppingCartItem` with the content of [{@}]", shoppingCart.Items.ToString());
             await connection.ExecuteAsync(
                 SqlQueries.addAllForShoppingCartSql,
                 shoppingCart.Items.Select(item => new
                 {
-                    shoppingCartId,
-                    item.ProductCatalogId,
-                    Productdescription = item.Description,
-                    item.ProductName,
-                    item.Price.Amount,
-                    item.Price.Currency
+                    ShoppingCartId = shoppingCartId,
+                    ProductCatalogId= item.ProductCatalogId,
+                    ProductName = item.ProductName,
+                    ProductDescription = item.Description,
+                    Amount = item.Price.Amount,
+                    Currency = item.Price.Currency
                 }), transaction
             );
             
