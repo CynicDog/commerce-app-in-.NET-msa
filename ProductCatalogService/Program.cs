4eafbc88054a04d9e06812252f81568ce3c3688f
ginsenglee <thecynicdog0328@gmail.com>
2023-11-28 14:54:03 +0900

    configured Serilog logger

--- ProductCatalogService/Program.cs
+++ ProductCatalogService/Program.cs
@@ -1,7 +1,18 @@
 using ProductCatalogService;
+using Serilog;
+using Serilog.Enrichers.Span;
 
 CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
+        .UseSerilog((context, logger) =>
+        {
+            logger
+                .Enrich.FromLogContext()
+                .Enrich.WithSpan()
+                .WriteTo.ColoredConsole(
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} {TraceId} {Level:u3} {Message}{NewLine}{Exception}"
+                );
+        })
         .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
--- ShoppingCartService/Program.cs
+++ ShoppingCartService/Program.cs
@@ -1,9 +1,20 @@
+using Serilog;
+using Serilog.Enrichers.Span;
 using ShoppingCartService;
 
 CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
+        .UseSerilog((context, logger) =>
+        {
+            logger
+                .Enrich.FromLogContext()
+                .Enrich.WithSpan()
+                .WriteTo.ColoredConsole(
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} {TraceId} {Level:u3} {Message}{NewLine}{Exception}"
+                );
+        })
         .ConfigureWebHostDefaults(webBuilder =>
         {
             webBuilder.UseStartup<Startup>();
